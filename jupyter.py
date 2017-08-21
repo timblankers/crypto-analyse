f1d795c7f2b6ed9a13b934effb77512185ddabe3
Tim Blankers <timblankers@gmail.com>
2017-08-21 22:44:12 +0200

    Initial jupyter notebook tryouts

---
+++ jupyter.py
@@ -0,0 +1,212 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import os
+import numpy as np
+import pandas as pd
+import pickle
+import quandl
+from datetime import datetime
+
+
+# In[4]:
+
+
+import plotly.offline as py
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+py.init_notebook_mode(connected=True)
+
+
+# In[5]:
+
+
+def get_quandl_data(quandl_id):
+    '''Download and cache Quandl dataseries'''
+    cache_path = '{}.pkl'.format(quandl_id).replace('/','-')
+    try:
+        f = open(cache_path, 'rb')
+        df = pickle.load(f)   
+        print('Loaded {} from cache'.format(quandl_id))
+    except (OSError, IOError) as e:
+        print('Downloading {} from Quandl'.format(quandl_id))
+        df = quandl.get(quandl_id, returns="pandas")
+        df.to_pickle(cache_path)
+        print('Cached {} at {}'.format(quandl_id, cache_path))
+    return df
+
+
+# In[7]:
+
+
+# Pull Kraken BTC price exchange data
+btc_usd_price_kraken = get_quandl_data('BCHARTS/KRAKENUSD')
+
+
+# In[8]:
+
+
+btc_usd_price_kraken.head()
+
+
+# In[9]:
+
+
+# Chart the BTC pricing data
+btc_trace = go.Scatter(x=btc_usd_price_kraken.index, y=btc_usd_price_kraken['Weighted Price'])
+py.iplot([btc_trace])
+
+
+# In[10]:
+
+
+# Pull pricing data for 3 more BTC exchanges
+exchanges = ['COINBASE','BITSTAMP','ITBIT']
+
+exchange_data = {}
+
+exchange_data['KRAKEN'] = btc_usd_price_kraken
+
+for exchange in exchanges:
+    exchange_code = 'BCHARTS/{}USD'.format(exchange)
+    btc_exchange_df = get_quandl_data(exchange_code)
+    exchange_data[exchange] = btc_exchange_df
+
+
+# In[11]:
+
+
+def merge_dfs_on_column(dataframes, labels, col):
+    '''Merge a single column of each dataframe into a new combined dataframe'''
+    series_dict = {}
+    for index in range(len(dataframes)):
+        series_dict[labels[index]] = dataframes[index][col]
+        
+    return pd.DataFrame(series_dict)
+
+
+# In[12]:
+
+
+# Merge the BTC price dataseries' into a single dataframe
+btc_usd_datasets = merge_dfs_on_column(list(exchange_data.values()), list(exchange_data.keys()), 'Weighted Price')
+
+
+# In[13]:
+
+
+btc_usd_datasets.tail()
+
+
+# In[15]:
+
+
+def df_scatter(df, title, seperate_y_axis=False, y_axis_label='', scale='linear', initial_hide=False):
+    '''Generate a scatter plot of the entire dataframe'''
+    label_arr = list(df)
+    series_arr = list(map(lambda col: df[col], label_arr))
+    
+    layout = go.Layout(
+        title=title,
+        legend=dict(orientation="h"),
+        xaxis=dict(type='date'),
+        yaxis=dict(
+            title=y_axis_label,
+            showticklabels= not seperate_y_axis,
+            type=scale
+        )
+    )
+    
+    y_axis_config = dict(
+        overlaying='y',
+        showticklabels=False,
+        type=scale )
+    
+    visibility = 'visible'
+    if initial_hide:
+        visibility = 'legendonly'
+        
+    # Form Trace For Each Series
+    trace_arr = []
+    for index, series in enumerate(series_arr):
+        trace = go.Scatter(
+            x=series.index, 
+            y=series, 
+            name=label_arr[index],
+            visible=visibility
+        )
+        
+        # Add seperate axis for the series
+        if seperate_y_axis:
+            trace['yaxis'] = 'y{}'.format(index + 1)
+            layout['yaxis{}'.format(index + 1)] = y_axis_config    
+        trace_arr.append(trace)
+
+    fig = go.Figure(data=trace_arr, layout=layout)
+    py.iplot(fig)
+# Plot all of the BTC exchange prices
+df_scatter(btc_usd_datasets, 'Bitcoin Price (USD) By Exchange')
+
+
+# In[16]:
+
+
+# Remove "0" values
+btc_usd_datasets.replace(0, np.nan, inplace=True)
+# Plot the revised dataframe
+df_scatter(btc_usd_datasets, 'Bitcoin Price (USD) By Exchange')
+
+
+# In[17]:
+
+
+# Calculate the average BTC price as a new column
+btc_usd_datasets['avg_btc_price_usd'] = btc_usd_datasets.mean(axis=1)
+# Plot the average BTC price
+btc_trace = go.Scatter(x=btc_usd_datasets.index, y=btc_usd_datasets['avg_btc_price_usd'])
+py.iplot([btc_trace])
+
+
+# In[18]:
+
+
+def get_json_data(json_url, cache_path):
+    '''Download and cache JSON data, return as a dataframe.'''
+    try:        
+        f = open(cache_path, 'rb')
+        df = pickle.load(f)   
+        print('Loaded {} from cache'.format(json_url))
+    except (OSError, IOError) as e:
+        print('Downloading {}'.format(json_url))
+        df = pd.read_json(json_url)
+        df.to_pickle(cache_path)
+        print('Cached {} at {}'.format(json_url, cache_path))
+    return df
+
+
+
+base_polo_url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period={}'
+start_date = datetime.strptime('2015-01-01', '%Y-%m-%d') # get data from the start of 2015
+end_date = datetime.now() # up until today
+pediod = 86400 # pull daily data (86,400 seconds per day)
+
+def get_crypto_data(poloniex_pair):
+    '''Retrieve cryptocurrency data from poloniex'''
+    json_url = base_polo_url.format(poloniex_pair, start_date.timestamp(), end_date.timestamp(), pediod)
+    data_df = get_json_data(json_url, poloniex_pair)
+    data_df = data_df.set_index('date')
+    return data_df
+
+altcoins = ['ETH','LTC','XRP','ETC','STR','DASH','SC','XMR','XEM']
+
+altcoin_data = {}
+for altcoin in altcoins:
+    coinpair = 'BTC_{}'.format(altcoin)
+    crypto_price_df = get_crypto_data(coinpair)
+    altcoin_data[altcoin] = crypto_price_df
+    
+altcoin_data['ETH'].tail()
+
